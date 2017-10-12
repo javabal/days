51a9e39a51e2de5f535aac26c4f6f8f950160016
javabal <geldiyevab@gmail.com>
2017-10-11 18:08:28 -0700

    12schet

---
+++ javabal/days.git/Day13/src/methods/BankAccount.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class BankAccount {
+
+	public static void main(String[] args) {
+
+		int accountNumber;
+		String accountHolder;
+		double balance;
+		String bank;
+
+		// public void setAccountNumber(int accNum);
+		// accountNumber = accNum;
+	}
+
+}
